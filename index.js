05401eafc0aed9805a30c49be9986c2750a3256a
Wagah <vinodinga@gmail.com>
2015-11-19 16:42:51 +0300

    intro to express

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ var http = require('http');
 var dispatch = require('dispatch');
 var querystring = require('querystring');
 
+//Express
+var express = require('express');
+var app = express();
+
+
+// express middleware
+var bodyParser = require('body-parser');
+
 //include mongoose
 
 var mongoose = require('mongoose');
@@ -12,100 +20,142 @@ mongoose.connect('mongodb://localhost/project-aardvark');
 
 //Define our schema
 var movieSchema = mongoose.Schema({
-		name:String,
-		year_of_relaese:Number
+		title: String,
+		year_of_release: Number
 
 })
 //Compile our model
 
 var Movie = mongoose.model('Movie', movieSchema);
 
+
+app.use(bodyParser.urlencoded({extended:true}));
+//the movie array which was initially here has been added in the databse using POSTMAN and then deleted.
+
+ app. get('/movies', function(req,res){
+ 	Movie.find(function(err,movie){
+ 		if(err){
+ 			console.log(err)
+ 		}else{
+
+			res.json(movie);
+
+ 		}
+
+ 	});
+
+ });
+
+//The following is the route of changing movie or adding a new
+ app.post('/movies/new',function(req, res){
+ 		console.log(req.body);
+ 		formData = req.body;
+ 		
+var movie = new Movie(formData);
+//save the movie, failure to the program should run an err then re-direct it to another route ie '/movies'
+
+movie.save(function(err,movie){
+	if(err){
+		console.log(err);
+	}else{
+
+		console.log('Sucessfully saved the movie');
+		res.redirect('/movies');
+		}
+		});
+
+
+ });
+
+
+
+
 // static database
-var server = http.createServer(
-				dispatch({
+// var server = http.createServer(
+// 				dispatch({
 					
-					'/movies' : {
+// 					'/movies' : {
 
-						'GET /' :function(request, response){
-							movies = [
-							{
-										title: 'Suits',
-										category: ['Drama','Investigative','Romance'],
-										main_actors: [{
-												first_name: 'Harvey',
-												second_name: 'Lawis'
+// 						'GET /' :function(request, response){
+// 							movies = [
+// 							{
+// 										title: 'Suits',
+// 										category: ['Drama','Investigative','Romance'],
+// 										main_actors: [{
+// 												first_name: 'Harvey',
+// 												second_name: 'Lawis'
 
-										},{
-												first_name: 'Micheal',
-												second_name:'Ross'
-										}]
-
-										
-									},
-									{
-										title: 'Constant Gardener',
-										category: ['Drama','Mystery','Romance'],
-										main_actors: [{
-												first_name: 'Maley',
-												second_name: 'Rick'
-										},
-										{
-												first_name:'Ralph',
-												second_name:'Davis'
-
-
-										}]
+// 										},{
+// 												first_name: 'Micheal',
+// 												second_name:'Ross'
+// 										}]
 
 										
-									},
-									{
-										title: 'Beyond the Horizon',
-										category: ['Biography','Drama','Romance'],
-										main_actors: [{
-												first_name: 'Maley',
-												second_name: 'Dowen'
-										},{}]
+// 									},
+// 									{
+// 										title: 'Constant Gardener',
+// 										category: ['Drama','Mystery','Romance'],
+// 										main_actors: [{
+// 												first_name: 'Maley',
+// 												second_name: 'Rick'
+// 										},
+// 										{
+// 												first_name:'Ralph',
+// 												second_name:'Davis'
+
+
+// 										}]
 
 										
-									}
-							]
+// 									},
+// 									{
+// 										title: 'Beyond the Horizon',
+// 										category: ['Biography','Drama','Romance'],
+// 										main_actors: [{
+// 												first_name: 'Maley',
+// 												second_name: 'Dowen'
+// 										},{}]
 
-							response.end(JSON.stringify(movies));
-						},
-						//Creating a dynamic database
-						'POST /':function(request,response){
-							//Get parameters from the form
-							formData = '',
-							request.on('data',function(chunk){
-								formData = querystring.parse(chunk.toString());
-							});
+										
+// 									}
+// 							]
+// 							console.log('Movies GET /')
+// 							response.end(JSON.stringify(movies));
+// 						},
+// 						//Creating a dynamic database
+// 						'POST /':function(request,response){
+// 							//Get parameters from the form
+// 							formData = '',
+// 							request.on('data',function(chunk){
+// 								formData = querystring.parse(chunk.toString());
+// 							});
 							
-							request.on('end', function(){
-								console.log(formData);
-							//Create an instance of movie
-								var movie = new Movie(
-								{
+// 							request.on('end', function(){
+// 								console.log(formData);
+// 							//Create an instance of movie
+// 								var movie = new Movie(
+// 								{
 
-								title: formData.title,
-								year_of_release: formData.year_of_release
-							}
-							);
+// 								title: formData.title,
+// 								year_of_release: formData.year_of_release
+// 							}
+// 							);
 
 							//Save the movie instance
 							//If successful the respond will be saved 
 
-								response.end('Movie was posted')
-							});
-						}
-					}
+				// 				response.end('Movie was posted')
+				// 			});
+				// 		}
+				// 	}
 
 
 
-				})
-			);
+				// })
+// 			);
 
-server.listen(8081,function(){
-	console.log('Server running on 127.0.0.1:8081');
+app.listen(8082,function(){
+	console.log('Server running on 127.0.0.1:8082');
 
 });
 
