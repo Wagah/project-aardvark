b46138f7c0929dc84529a16a34d2863775b28911
Wagah <vinodinga@gmail.com>
2015-12-02 16:10:42 +0300

    Updating and deleting data by sending  http request

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 //Express
 var express = require('express');
 var app = express();
@@ -16,77 +15,109 @@ mongoose.connect('mongodb://localhost/project-aardvark');
 
 //Define our schema
 var movieSchema = mongoose.Schema({
-		title: String,
-		year_of_release: Number
+    title: String,
+    year_of_release: Number,
+    rating: {type: Number, default: 0, min: 0, max: 10}
+    //the schema is first updated before sending the update request
 
 })
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 //Compile our model
 
 var Movie = mongoose.model('Movie', movieSchema);
 
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 //the movie array which was initially here has been added in the databse using POSTMAN and then deleted.
 
- app. get('/movies', function(req,res){
- 	Movie.find(function(err,movie){
- 		if(err){
- 			console.log(err)
- 		}else{
+app.get('/movies', function(req, res) {
+    Movie.find(function(err, movie) {
+        if (err) {
+            console.log(err)
+        } else {
 
-			res.json(movie);
+            res.json(movie);
 
- 		}
+        }
 
- 	});
+    });
 
- });
+});
 
 //The following is the route of changing movie or adding a new
- app.post('/movies/new',function(req, res){
- 		console.log(req.body);
- 		formData = req.body;
- 		
-var movie = new Movie(formData);
-//save the movie, failure to the program should run an err then re-direct it to another route ie '/movies'
+app.post('/movies/new', function(req, res) {
+    console.log(req.body);
+    formData = req.body;
 
-movie.save(function(err,movie){
-	if(err){
-		console.log(err);
-	}else{
+    var movie = new Movie(formData);
+    //save the movie, failure to the program should run an err then re-direct it to another route ie '/movies'
 
-		console.log('Sucessfully saved the movie');
-		res.redirect('/movies');
-		}
-		});
+    movie.save(function(err, movie) {
+        if (err) {
+            console.log(err);
+        } else {
+
+            console.log('Sucessfully saved the movie');
+            res.redirect('/movies');
+        }
+    });
 
 
- });
+});
 
 app.get('/movies/:id', function(req, res) {
-  movieId = req.params.id;
+    movieId = req.params.id;
 
-//Retrieve the movie from mongodb- we gona require mongoose
-Movie.findById(movieId, function (err, movie) {
-	if(err) return console.log(err);
+    //Retrieve the movie from mongodb- we gona require mongoose
+    Movie.findById(movieId, function(err, movie) {
+        if (err) return console.log(err);
 
-	res.json(movie);
+        res.json(movie);
 
-	});
-
-
+    });
 
 });
 
+//Performing an updata in the database: At first we added the updated element in the schema then proceed
+//to send a http request using PUT request
+app.put('/movies/:id', function(req, res) {
+    movieId = req.params.id;
+    userRating =req.body.rating;
 
-			
-app.listen(8082,function(){
-	console.log('Server running on 127.0.0.1:8082');
+    //Retrieve the movie from mongodb- we gona require mongoose
+    Movie.findById(movieId, function(err, movie) {
+        if (err) return console.log(err);
+
+        movie.rating = userRating;
+
+        movie.save(function(err,movie){
+        	if(err) return console.log(err);
+        	res.json(movie);
+        });
+
+    });
 
 });
 
+app.delete('/movies/:id', function(req, res) {
+    movieId = req.params.id;
+
+    //Retrieve the movie from mongodb- we gona require mongoose
+    Movie.remove({_id:movieId}, function(err){
+    	if(err) return console.log(err);
+    	res.send("Movie was deleted");
+
+   	 });
+   });
+
+
+app.listen(8082, function() {
+    console.log('Server running on 127.0.0.1:8082');
+
+});
